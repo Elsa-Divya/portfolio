45aea9e4c86db6db9b641c999c628c872ef04a26
Divya Elsa <divyaelsaaagnes@gmail.com>
2018-11-12 12:39:40 +0530

    add aboout me

--- js/script.js
+++ js/script.js
@@ -10,4 +10,17 @@ $(document).ready(function(){
         $(".active").removeClass("active");
         $(this).addClass("active");
     })
+
+    $(".progress").hover(function(){
+        $(this).toggleClass("slideInUp");
+    });
+
+   var skillsprogressbar = $(".progress-bar-striped");
+   $.each(skillsprogressbar,function(index,bar){
+       var actualValue = $(this).attr("style").split(":")[1].split("%")[0];
+       var initialValue = "0";
+       
+       
+       
+   })
 })
